742047469ab0bc6e15b5a656a38ca68bbc1fdf26
Riddhesh Sajwan <riddhesh10@gmail.com>
2021-06-05 21:38:16 +0530

    Doubly Linked List and minor fixes in Singly Linked List

--- linked-list/doublyLinkedList.py
+++ linked-list/doublyLinkedList.py
@@ -2,8 +2,9 @@ class Node:
     def __init__(self,value=None):
         self.value = value
         self.next = None
+        self.prev = None
 
-class singlyLinkedList():
+class doublyLinkedList():
 
     def __init__(self,node=None):
         self.head = node
@@ -12,11 +13,13 @@ class singlyLinkedList():
     
     def append(self,node):
         self.tail.next = node
+        node.prev = self.tail
         self.tail = node
         self.length += 1
         
     def preprend(self,node):
         node.next = self.head
+        self.head.prev = node
         self.head = node
         self.length += 1
 
@@ -34,7 +37,9 @@ class singlyLinkedList():
             self.preprend(node)
             return
         currentNode = self.traverseToIndex(index)
+        currentNode.next.prev = node
         node.next = currentNode.next
+        node.prev = currentNode
         currentNode.next = node
         self.length += 1
 
@@ -43,47 +48,60 @@ class singlyLinkedList():
         return currentNode.next.value
     
     def remove(self,index):
-        
         if index == 0:
             self.head = self.head.next
+            self.head.prev = None
             self.length -= 1
             return
         currentNode = self.traverseToIndex(index)
-        nodeToDelete = currentNode.next 
+        nodeToDelete = currentNode.next  
         if nodeToDelete == self.tail:
             self.tail = currentNode
+            self.tail.next = None
+            self.length -= 1
+            return      
         currentNode.next = nodeToDelete.next
+        nodeToDelete.next.prev = currentNode
         self.length -= 1
-
+    
     def reverse(self):
-        if self.length == 1:
-            return
         prevNode = self.head
         currentNode = self.head.next
         self.head.next = None
-        self.tail = self.head
+        self.head.prev = self.tail.prev
         while currentNode:
             nextNode = currentNode.next
             currentNode.next = prevNode
+            currentNode.prev = nextNode
             prevNode = currentNode
             currentNode = nextNode
-        self.head = prevNode
-             
+        self.tail = prevNode
+        self.head = self.tail
+
+    def printReverse(self):
+        dll = ''
+        currentNode = self.tail
+        while currentNode.prev:
+            dll += str(currentNode.value) + '<->'
+            currentNode = currentNode.prev
+        dll += str(currentNode.value)
+        print(dll)
 
     def printList(self):
-        sll = ''
+        dll = ''
         currentNode = self.head
         while currentNode.next:
-            sll += str(currentNode.value) + '->'
+            dll += str(currentNode.value) + '<->'
             currentNode = currentNode.next
-        sll += str(currentNode.value)
-        print(sll)
+        dll += str(currentNode.value)
+        print(dll)
 
-myLinkedList = singlyLinkedList(Node(24))
+myLinkedList = doublyLinkedList(Node(24))
 myLinkedList.append(Node(25))
 myLinkedList.preprend(Node(23))
-myLinkedList.insert(1,Node(35))
+myLinkedList.insert(2,Node(35))
 myLinkedList.printList()
+myLinkedList.printReverse()
 myLinkedList.reverse()
 myLinkedList.printList()
 print(myLinkedList.access(2))
