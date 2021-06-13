add14523177fd3cf7dafa857d657b704e5db8d0f
Riddhesh Sajwan <riddhesh10@gmail.com>
2021-06-13 23:05:07 +0530

    Find, Count and maxDepth function for Binary Tree

--- tree/binarySearchTree.py
+++ tree/binarySearchTree.py
@@ -24,6 +24,7 @@ class binarySearchTree:
                     currentNode.right = node   
                     return       
 
+# find method for bst.
     def find(self,value):
         currentNode = self.root
         while True:
@@ -40,6 +41,50 @@ class binarySearchTree:
             else:
                 return True
 
+    def countNodes(self):
+        if not self.root:
+            return 0
+        def _countNodes(currentNode):
+            if currentNode.left and currentNode.right:
+                return 1 +  _countNodes(currentNode.left) + _countNodes(currentNode.right)
+            elif currentNode.right:
+                return 1 + _countNodes(currentNode.right)
+            elif currentNode.left:
+                return 1 + _countNodes(currentNode.left)
+            return 1
+        return _countNodes(self.root)
+
+# find method for any binary tree
+    def findRecursive(self,value):
+        if self.root.value == value:
+            return True
+        def _findRecusrsive(currentNode):
+            if currentNode.value == value:
+                return True
+            elif currentNode.right and currentNode.left:
+                return _findRecusrsive(currentNode.left) or _findRecusrsive(currentNode.right)
+            elif currentNode.right:
+                return _findRecusrsive(currentNode.right)
+            elif currentNode.left:
+                return _findRecusrsive(currentNode.left)
+            return False
+        return _findRecusrsive(self.root)
+
+   #assuming root to be at height 1.  
+    def maxDepth(self):
+        if not self.root:
+            return 0
+        def _maxDepth(currentNode,height=0):
+            if currentNode == self.root:
+                height = 1
+            if currentNode.right and currentNode.left:
+                return height + max(_maxDepth(currentNode.left,height),_maxDepth(currentNode.right,height))
+            elif currentNode.right:
+                return height + _maxDepth(currentNode.right,height)
+            elif currentNode.left:
+                return height + _maxDepth(currentNode.left,height)
+            return height
+        return _maxDepth(self.root)
 
 myTree = binarySearchTree(Node(23))
 myTree.insert(Node(15))
@@ -48,3 +93,6 @@ myTree.insert(Node(16))
 myTree.insert(Node(24))
 myTree.insert(Node(23))
 print(myTree.find(69))
+print(myTree.countNodes())
+print(myTree.findRecursive(14))
+print(myTree.maxDepth())
