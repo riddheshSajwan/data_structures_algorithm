8d35db0575ccf9559d42de6322b4b95f43ea944c
Riddhesh Sajwan <riddhesh10@gmail.com>
2022-05-11 23:25:03 +0530

    Greedy Algorithm

---
+++ greedy/finishMaximumJobs.py
@@ -0,0 +1,58 @@
+'''
+Problem Description
+There are N jobs to be done, but you can do only one job at a time.
+Given an array A denoting the start time of the jobs and an array B denoting the finish time of the jobs.
+Your aim is to select jobs in such a way so that you can finish the maximum number of jobs.
+Return the maximum number of jobs you can finish.
+
+Problem Constraints
+1 <= N <= 105
+1 <= A[i] < B[i] <= 109
+
+Input Format
+The first argument is an integer array A of size N, denoting the start time of the jobs.
+The second argument is an integer array B of size N, denoting the finish time of the jobs.
+
+Output Format
+Return an integer denoting the maximum number of jobs you can finish.
+
+Example Input
+Input 1:
+ A = [1, 5, 7, 1]
+ B = [7, 8, 8, 8]
+Input 2:
+ A = [3, 2, 6]
+ B = [9, 8, 9]
+
+
+Example Output
+Output 1:
+ 2
+Output 2:
+ 1
+
+Example Explanation
+Explanation 1:
+ We can finish the job in the period of time: (1, 7) and (7, 8).
+Explanation 2:
+ Since all three jobs collide with each other. We can do only 1 job.
+ '''
+
+class Solution:
+    # @param A : list of integers
+    # @param B : list of integers
+    # @return an integer
+    def solve(self, A, B):
+        jobs = []
+        for i in range(len(A)):
+            jobs.append((A[i],B[i]))
+        jobs.sort(key = lambda x: x[1])
+        elapsed_time = 0
+        res = 0
+        for job in jobs:
+            if job[0] >= elapsed_time:
+                res += 1
+                elapsed_time = job[1]
+        return res
+            
+print(Solution().solve([1, 5, 7, 1], [7, 8, 8, 8]))
